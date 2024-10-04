c3aa4e6d73e988c6f026e862f6edc154284321a1
LPAnhKhôi <lekhoi1492@gmail.com>
2024-10-04 09:57:41 +0700

    38

--- express/db.js
+++ express/db.js
@@ -15,7 +15,8 @@ import mysql from 'mysql2/promise'
 import dotenv from 'dotenv';
 dotenv.config();
 
-	const connect = mysql.createPool(
+	const connect = mysql.createPool
+	(
 		{
 			host: process.env.DB_HOST,
 			user: process.env.DB_USER,
