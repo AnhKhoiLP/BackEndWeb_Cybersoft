4ed2b9122b995015b84781a41266eb0c879ac7a1
LPAnhKhôi <lekhoi1492@gmail.com>
2024-10-31 13:44:13 +0700

    55

--- 01_In_Class_Code/Buoi_04_Express/database.js
+++ 01_In_Class_Code/Buoi_04_Express/database.js
@@ -1,13 +1,34 @@
-
-import mysql from 'mysql2/promise'
-
-const	connect = mysql.createPool
-({
-	host: "localhost",
-	user: "root",
-	password: "123456",
-	database: "app_food",
-	port: "3306"
-})
-
-export default connect;
+	// import mysql from 'mysql2/promise'
+	// const	connect = mysql.createPool
+	// ({
+	// 	host: "localhost",
+	// 	user: "root",
+	// 	password: "123456",
+	// 	database: "app_food",
+	// 	port: "3306"
+	// })
+	// export default connect;
+//> Thay Đổi Cách Import Để Giảm Dung Lượng
+	// import {createPool} from 'mysql2/promise'
+	// const	connect = createPool
+	// ({
+	// 	host: "localhost",
+	// 	user: "root",
+	// 	password: "123456",
+	// 	database: "app_food",
+	// 	port: "3306"
+	// })
+	// export default connect;
+//> Sau Khi Tạo File .env Dùng Đoạn Script Sau
+	import {createPool} from 'mysql2/promise'
+	import dotenv from 'dotenv'
+	dotenv.config();
+	const connect = createPool
+	({
+		host: DB_HOST,
+		user: DB_USER,
+		password: DB_PASS,
+		port: DB_PORT,
+		database: DB_DATABASE
+	});
+	export default connect;
