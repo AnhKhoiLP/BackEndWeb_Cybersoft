a0004f765d588f69e1bd4def6d7ba0fd2ef95cdb
LPAnhKhôi <lekhoi1492@gmail.com>
2024-11-01 12:24:17 +0700

    62

--- 01_In_Class_Code/Buoi_05_MVC_SquelizeORM/index02.js
+++
@@ -1,9 +0,0 @@
-//| Tạo Folder src
-	//+ Tạo Folder routes
-		//* Tạo File rootRoutes.js
-			//$ Tiếp Tục Tại rootRoutes.js
-		//* Tạo File userRoutes.js
-		//* Tạo File videoRoutes.js
-	//+ Tạo Folder controllers
-	//+ Tạo Folder models
-	//+ Tạo Folder config
