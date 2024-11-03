dce7bc75b4e9dc480a190c12469abd3d30ede924
LPAnhKhôi <lekhoi1492@gmail.com>
2024-11-03 16:35:25 +0700

    71

--- 02_Homework/Buoi_10_Sequelize_App_Food/BE/0_index.js
+++ 02_Homework/Buoi_10_Sequelize_App_Food/BE/0_index.js
@@ -6,13 +6,19 @@ app.listen(8080, () =>
 	{
 		console.log("BE Starting With Port 8080");
 	})
-app.get("/get-user-from-database", async (request, response) =>
+
+//+ Get User From Database 
+	app.get("/get-user-from-database", async (request, response) =>
 	{
 		const[data] = await connect.query
 		(`
 			SELECT
-				*
+				user_id AS "User ID",
+				full_name As "Full Name",
+				email AS "User Emall",
+				pass_word AS "Password"
 			FROM users
 		`)
 		response.send(data);
 	})
+	//> http://localhost:8080/get-user-from-database
